7b8e7f618bb2ff185773388975b3a5ebf0522081
Nico van Duijn <nico.vanduijn@zuehlke.com>
2024-11-26 14:14:54 +0100

    Initial hacky solution

--- platform/stm32mp1/driver/src/init.cpp
+++ platform/stm32mp1/driver/src/init.cpp
@@ -2,6 +2,7 @@
 
 #include "platform/stm32mp1/driver/inc/gpio_driver.hpp"
 #include "platform/stm32mp1/driver/inc/mpu6050_driver.hpp"
+#include "platform/stm32mp1/driver/inc/uart_module.hpp"
 
 namespace Driver {
 
@@ -10,6 +11,7 @@ Common::Interface::Drivers init_low_level(void) {
 
     static Driver::Gpio runled{Common::Interface::Functionality::RUN_LED};
     static Driver::Mpu6050 imu{Common::Interface::Functionality::IMU};
+    static UARTModule uart{imu};
 
     static Common::Interface::Drivers drivers{runled, imu};
     return drivers;
